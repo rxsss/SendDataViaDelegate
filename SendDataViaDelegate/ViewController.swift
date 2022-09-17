7b834996fa1c4c212eb4460b2248d5eed7274eec
Rakhman <rmagametov@one.kz>
2022-09-17 18:51:42 +0600

    Add label and button

--- SendDataViaDelegate/ViewController.swift
+++ SendDataViaDelegate/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var sayHiButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        titleLabel.text = "Welcome!"
+        sayHiButton.layer.cornerRadius = 5
     }
 
+    @IBAction func sayHiDidTapped(_ sender: UIButton) {
+        titleLabel.text = "HI!"
+        sayHiButton.layer.cornerRadius += 1
+    }
+    
 }
 
